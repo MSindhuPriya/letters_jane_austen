a07c184bf833dfdffc8ca3bbcd4f335c84a5a1c4
MSindhuPriya <sindhupriya.mallavarapu@mail.utoronto.ca>
2024-03-15 23:21:53 -0400

    Update tests

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -13,3 +13,18 @@ library(tidyverse)
 # [...UPDATE THIS...]
 
 #### Test data ####
+
+test_data <- read.csv("outputs/data/analysis_data.csv")
+
+# make sure that we cleaned the data properly so that number of e's is an integer
+test_data$count_e |> class() == 'integer'
+
+# make sure we cleaned the data properly so that the word count is an integer
+test_data$word_count |> class() == 'integer'
+
+# make sure that we are only analyzing the lines where there are words
+test_data$word_count |> min() > 0
+
+test_data$count_e |> min() >= 0
+
+
